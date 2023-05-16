1df43d3c55f58c15b5992bd0af0d62656dea08ec
Daniek <info@daniek.io>
2023-05-16 10:21:45 +0200

    added task.js

---
+++ models/task.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        required: true,
+        enum: [pending,
+            inProgress,
+            completed],
+        type: String,
+        default: pending,
+
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    deadline: {
+        type: Date,
+    }
+});
+
+export const Task = model('Task', taskSchema);
